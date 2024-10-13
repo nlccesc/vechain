c3a3aeb2eb7e93f334d8a8dbaac197924e75dbd2
Blankeeir <e1300538@u.nus.edu>
2024-10-13 10:48:52 +0800

    update images

---
+++ apps/backend/src/routes/auth.route.ts
@@ -0,0 +1,3 @@
+// this route is for user authentications. for business owners, they can create an account, login, and logout.
+// for users, they can connect with wallet, or log in using username and password. if connecting for the first time, create a username
+// and password. if logging in, use the username and password.
---
+++ apps/backend/src/routes/transaction.route.ts
@@ -0,0 +1,4 @@
+// transaction is done peer to peer on vechain blockchain
+// transaction is done through smart contract
+// users can use their wallet to send and receive transactions
+// wallets support multiple currencies including fiat and crypto
